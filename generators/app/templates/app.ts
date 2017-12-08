25af1dfc24a21b24183d70dbaa0b83f079069800
Khaliq Gant <khaliqgant@gmail.com>
2017-12-08 10:35:21 -0500

    quick fixes and readme update

--- generators/app/templates/app.ts
+++ generators/app/templates/app.ts
@@ -1,33 +1,33 @@
 /// <reference path="./typings/tsd.d.ts"/>
 
-import {Request, Response} from "express";
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+import { Request, Response } from "express";
+const express = require("express");
+const path = require("path");
+const favicon = require("serve-favicon");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 
-var routes = require('./routes/index');
-var app = express();
+const routes = require("./routes/index");
+const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', routes);
+app.use("/", routes);
 
 // catch 404 and forward to error handler
 app.use((req: Request, res: Response, next: Function) => {
-  var err: any = new Error('Not Found');
+  var err: any = new Error("Not Found");
   err.status = 404;
   next(err);
 });
@@ -36,10 +36,10 @@ app.use((req: Request, res: Response, next: Function) => {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
+if (app.get("env") === "development") {
   app.use(function(err: any, req: Request, res: Response, next: Function) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.render("error", {
       message: err.message,
       error: err
     });
@@ -50,11 +50,10 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err: any, req: Request, res: Response, next: Function) {
   res.status(err.status || 500);
-  res.render('error', {
+  res.render("error", {
     message: err.message,
     error: {}
   });
 });
-
 
 module.exports = app;
